2798e9449cf1a51504b03da8f06f8f7cdfa62848
Raziel <razzziel001@gmail.com>
2019-07-07 21:50:53 -0500

    Tabla con entidades producto, cliente  y pedido como principales

---
+++ mi_primera_tabla.sql
@@ -0,0 +1,28 @@
+CREATE TABLE productos(
+        id_producto INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id_producto),
+    	nombre VARCHAR(150),
+    	precio int
+
+   );
+
+CREATE TABLE clientes(
+    id_cliente INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id_cliente),
+    nombre VARCHAR (100),
+    apellido VARCHAR (100),
+    edad int,
+    telefono int
+    );
+
+CREATE TABLE pedidos(
+    id_pedido INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id_pedido),
+    fecha DATE,
+    cantidad INT,
+    id_cliente INT,
+    id_producto INT,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+
+
+    );
